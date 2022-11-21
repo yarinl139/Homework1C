2220a9aac37cfa666948b73a9fa40f21cbf80a54
Yarin Levi <85311237+yarinl139@users.noreply.github.com>
2022-11-21 19:34:28 +0200

    fix

--- main.c
+++ main.c
@@ -6,39 +6,39 @@ int main()
 
     scanf("%d %d",&x , &y);
     
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i=x;i<=y;i++)
     {
         if (isArmstrong(i))
         {
-        printf("%d ", i);
+        printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for(int i=x;i<=y;i++)
     {
         if (isPalindrome(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i=x;i<=y;i++)
     {
         if (isPrime(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for(int i=x;i<=y;i++)
     {
         if (isStrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
